ae1d62578dec6fccfb2c80235314218a67af46dc
cornelisvd <vanduyvendijk@gmail.com>
2014-08-28 18:54:00 +0200

    install script

--- R/.PackageInstall.R
+++ R/.PackageInstall.R
@@ -2,23 +2,23 @@
 ## This script can check for required packages and, when required, instal these.
 ##--------------------------------v.28-08-14----------------------------------##
 
-# Add names of required packages here
-
-    required.packages <- c(     "ggplot2",                       
-                                "gstat",
-                                "lubridate",
-                                "raster",
-                                "rgeos",
-                                "sp",
-                                "spacetime",
-                                "splancs",
-                                "zoo",
-                                "solaR"
-                          )
-
-# Check/install required packages (from Sacha Epskamp at http://bit.ly/1pLPafb)
+# Function to install the required packages in the project
+    pkgInstall <- function(pkg) {
+        
+        ## Add names of required packages here:
+        
+            required.packages <- c(     "ggplot2",                       
+                                        "gstat",
+                                        "lubridate",
+                                        "raster",
+                                        "rgeos",
+                                        "sp",
+                                        "spacetime",
+                                        "splancs",
+                                        "zoo"
+                                   )
     
-    .pkgInstall <- function(pkg) {
+    ## Check/install the packages (from Sacha Epskamp at http://bit.ly/1pLPafb)
         for (pkg in required.packages)    
         {
             if (!require(pkg,character.only = TRUE))
@@ -27,4 +27,4 @@
             if(!require(pkg,character.only = TRUE)) stop("Package not found")
             }
         }    
-    }    
+    }     
