bf638a841464e327670dc639875802ea6f644c39
cornelisvd <vanduyvendijk@gmail.com>
2014-09-02 15:04:43 +0200

    experiment 1 text+code

--- R/Exp1.R
+++ R/Exp1.R
@@ -25,9 +25,10 @@
     {
        
     ## 
+        require(zoo)
         start    <- as.POSIXct("23/06/14 12:00:00", format = "%d/%m/%y %H:%M:%S")
         end      <- as.POSIXct("30/06/14 11:59:00", format = "%d/%m/%y %H:%M:%S")
-        season   <- seq(from = start, to = end, 
+        season   <<- seq(from = start, to = end, 
                         by = as.difftime(1, units = "mins"))  
         index    <- zoo(1, season) 
         seas     <- index
@@ -73,5 +74,36 @@
         exp.time(type = exp.obs[l], unit = "Humd")
         exp.humd[[l]] <- est.obs
     }      
+    
+    names(exp.temp) <- exp.obs
+    names(exp.humd) <- exp.obs
+    
         rm(exp.obs, k, l)
     
+    
+    plotlayers <- function(ylabel, ylimit, title) {
+    p <- ggplot(layer, aes(x=season, y=layer[,1])) + 
+        geom_point(alpha = 0.02, size = 0.01) + 
+        xlab("Date") + ylab(ylabel) + ylim(ylimit) + ggtitle(title) +
+        theme(legend.position="none", panel.background = element_rect(fill = 'white'),
+              panel.border = element_rect(color="black", size = 0.2, fill = NA),
+              plot.title = element_text(vjust=1.8, face="bold"),
+              axis.title.x=element_text(vjust=0.01))
+         for (i in 2:length(layer)) {
+        p <- p + geom_point(y= layer[,i], colour = "black", alpha = 0.02)
+        }
+    return(p)
+    }
+    
+    layer <- exp.temp$Type1
+    plotlayers(ylabel = "degrees Celsius", ylimit = c(15, 30), 
+               title = "Type 1 (lower resolution) temperature observations") 
+    layer <- exp.temp$Type2
+    plotlayers(ylabel = "degrees Celsius", ylimit = c(15, 30),
+               title = "Type 2 (higher resolution) temperature observations") 
+    layer <- exp.humd$Type1
+    plotlayers(ylabel = "relative humidity (%)", ylimit = c(50, 100),
+               title = "Type 1 (lower resolution) humidity observations") 
+    layer <- exp.humd$Type2
+    plotlayers(ylabel = "relative humidity (%)", ylimit = c(50, 100),
+               title = "Type 2 (higher resolution) humidity observations") 
