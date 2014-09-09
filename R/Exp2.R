7d1059f0d497b1e1eeaf1f0c64f2bcd905689370
cornelisvd <vanduyvendijk@gmail.com>
2014-09-09 10:52:30 +0200

    update experiment 2

--- R/Exp2.R
+++ R/Exp2.R
@@ -33,7 +33,7 @@ source(".Exp2Functions.R")
 
     plotlayers <- function() {
         p <- ggplot(test1, aes(x=season, y=test1[,1], color = "station temperature")) + 
-            geom_line() + ggtitle("Temperature and humidity in pipes vs. station") + 
+            geom_line() + ggtitle("Temperature and humidity in the pipes vs. the station") + 
              ylab("Temperature (degrees Celsius) and relative humidity (%)") + xlab("Date") +
             theme(panel.background = element_rect(fill = 'white'),
                   panel.border = element_rect(color="black", size = 0.2, fill = NA),
@@ -42,11 +42,207 @@ source(".Exp2Functions.R")
                   geom_line(aes(y = test1[,2], color = "humidity (station)")) +
                   geom_line(aes(y = test1[,3], color = "temperature (pipes)"),
                             linetype="longdash") + ylim(c(0,100)) +
-                  geom_line(aes(y = test1[,4], color = "humidity(pipes)"),
+                  geom_line(aes(y = test1[,4], color = "humidity (pipes)"),
                             linetype="longdash") +
                  scale_color_manual(name  ="Legend", 
                            values=c("blue", "lightblue", "red", "orange")) 
                   return(p)
     }
     plotlayers()
-##------------------------------------------------------------------------------------##
+##------------------------------2nd test (large tubes)------------------------------------##
+
+  exp.time4(exp = "Exp3", type = "Station", unit = "Temp")
+    test2.station.temp <- as.matrix(est.obs)
+  exp.time4(exp = "Exp3", type = "White", unit = "Temp")
+    test2.white.temp <- as.matrix(est.obs)  
+  exp.time4(exp = "Exp3", type = "Foil - no holes", unit = "Temp")
+    test2.foilnh.temp <- as.matrix(est.obs)  
+  exp.time4(exp = "Exp3", type = "Foil - with holes", unit = "Temp")
+    test2.foilwh.temp <- as.matrix(est.obs)
+  exp.time4(exp = "Exp3", type = "Holes", unit = "Temp")
+    test2.holes.temp <- as.matrix(est.obs)
+  exp.time4(exp = "Exp3", type = "Reflective tape", unit = "Temp")
+    test2.reflect.temp <- as.matrix(est.obs)
+      t2.s.t <- rowMeans(test2.station.temp)
+      t2.w.t <- rowMeans(test2.white.temp)
+      t2.n.t <- rowMeans(test2.foilnh.temp)
+      t2.f.t <- rowMeans(test2.foilwh.temp)
+      t2.h.t <- rowMeans(test2.holes.temp)
+      t2.r.t <- rowMeans(test2.reflect.temp)
+    
+      test2 <- data.frame(t2.s.t, t2.w.t, t2.n.t, t2.f.t, t2.h.t, t2.r.t)
+
+    plotlayers <- function() {
+      p <- ggplot(test1, aes(x=season, y=test2[,1], color = "a. Station")) + 
+        geom_line() + ggtitle("Temperature in different 50mm constructions (sunny day)") + 
+        ylab("Temperature (degrees Celsius)") + xlab("Date") +
+        theme(panel.background = element_rect(fill = 'white'),
+              panel.border = element_rect(color="black", size = 0.2, fill = NA),
+              plot.title = element_text(vjust=1.8, face="bold"),
+              axis.title.x=element_text(vjust=0.01)) +
+        geom_line(aes(y = test2[,2], color = "b. White paint")) +
+        geom_line(aes(y = test2[,3], color = "c. Foil - with holes"),
+                  linetype="longdash") + ylim(c(15,35)) +
+        geom_line(aes(y = test2[,4], color = "d. Foil - no holes"),
+                  linetype="longdash") +
+        geom_line(aes(y = test2[,5], color = "e. Holes"),
+                  linetype="longdash") +
+        geom_line(aes(y = test2[,6], color = "f. Reflective tape"),
+                  linetype="longdash") +
+        scale_color_manual(name  ="Legend", 
+                  values=c("red", "lightblue", "blue", "grey75", "green", "lightgreen")) 
+      return(p)
+    }
+    plotlayers()
+
+##------------------------------3rd test (large tubes)------------------------------------##
+
+  exp.time4(exp = "Exp4", type = "Station", unit = "Temp", start.m  = "06/07/14 00:00:00",
+            end.m    = "06/07/14 23:59:00")
+    test3.station.temp <- as.matrix(est.obs)
+  exp.time4(exp = "Exp4", type = "White", unit = "Temp", start.m  = "06/07/14 00:00:00",
+            end.m    = "06/07/14 23:59:00")
+    test3.white.temp <- as.matrix(est.obs)  
+  exp.time4(exp = "Exp4", type = "Foil - no holes", unit = "Temp", start.m  = "06/07/14 00:00:00",
+            end.m    = "06/07/14 23:59:00")
+    test3.foilnh.temp <- as.matrix(est.obs)  
+  exp.time4(exp = "Exp4", type = "Foil - with holes", unit = "Temp", start.m  = "06/07/14 00:00:00",
+            end.m    = "06/07/14 23:59:00")
+    test3.foilwh.temp <- as.matrix(est.obs)
+  exp.time4(exp = "Exp4", type = "Holes", unit = "Temp", start.m  = "06/07/14 00:00:00",
+            end.m    = "06/07/14 23:59:00")
+    test3.holes.temp <- as.matrix(est.obs)
+  exp.time4(exp = "Exp4", type = "Reflective tape", unit = "Temp",start.m  = "06/07/14 00:00:00",
+            end.m    = "06/07/14 23:59:00")
+    test3.reflect.temp <- as.matrix(est.obs)
+      t3.s.t <- rowMeans(test3.station.temp)
+      t3.w.t <- rowMeans(test3.white.temp)
+      t3.n.t <- rowMeans(test3.foilnh.temp)
+      t3.f.t <- rowMeans(test3.foilwh.temp)
+      t3.h.t <- rowMeans(test3.holes.temp)
+      t3.r.t <- rowMeans(test3.reflect.temp)
+  
+  test3 <- data.frame(t3.s.t, t3.w.t, t3.n.t, t3.f.t, t3.h.t, t3.r.t)
+  
+  plotlayers <- function() {
+    p <- ggplot(test1, aes(x=season, y=test3[,1], color = "a. Station")) + 
+      geom_line() + ggtitle("Temperature in different 50mm constructions (rainy day)") + 
+      ylab("Temperature (degrees Celsius)") + xlab("Date") +
+      theme(panel.background = element_rect(fill = 'white'),
+            panel.border = element_rect(color="black", size = 0.2, fill = NA),
+            plot.title = element_text(vjust=1.8, face="bold"),
+            axis.title.x=element_text(vjust=0.01)) +
+      geom_line(aes(y = test3[,2], color = "b. White paint")) +
+      geom_line(aes(y = test3[,3], color = "c. Foil - with holes"),
+                linetype="longdash") + ylim(c(15,35)) +
+      geom_line(aes(y = test3[,4], color = "d. Foil - no holes"),
+                linetype="longdash") +
+      geom_line(aes(y = test3[,5], color = "e. Holes"),
+                linetype="longdash") +
+      geom_line(aes(y = test3[,6], color = "f. Reflective tape"),
+                linetype="longdash") +
+      scale_color_manual(name  ="Legend", 
+                values=c("red", "lightblue", "blue", "grey75", "green", "lightgreen"))  
+    return(p)
+  }
+  plotlayers()
+                         
+##------------------------------4th test (small tubes)------------------------------------##
+
+exp.time4(exp = "Exp5", type = "Station", unit = "Temp", start.m  = "09/07/14 15:00:00",
+          end.m    = "10/07/14 14:59:00")
+test4.station.temp <- as.matrix(est.obs)
+exp.time4(exp = "Exp5", type = "Holes", unit = "Temp", start.m  = "09/07/14 15:00:00",
+          end.m    = "10/07/14 14:59:00")
+test4.holes.temp <- as.matrix(est.obs)  
+exp.time4(exp = "Exp5", type = "Holesfoil", unit = "Temp", start.m  = "09/07/14 15:00:00",
+          end.m    = "10/07/14 14:59:00")
+test4.foilwh.temp <- as.matrix(est.obs)  
+exp.time4(exp = "Exp5", type = "Nothing", unit = "Temp", start.m  = "09/07/14 15:00:00",
+          end.m    = "10/07/14 14:59:00")
+test4.nothing.temp <- as.matrix(est.obs)
+exp.time4(exp = "Exp5", type = "Foil", unit = "Temp", start.m  = "09/07/14 15:00:00",
+          end.m    = "10/07/14 14:59:00")
+test4.foil.temp <- as.matrix(est.obs)
+
+t4.s.t  <- rowMeans(test4.station.temp)
+t4.h.t  <- rowMeans(test4.holes.temp)
+t4.fh.t <- rowMeans(test4.foilwh.temp)
+t4.n.t  <- rowMeans(test4.nothing.temp)
+t4.f.t  <- rowMeans(test4.foil.temp)
+
+
+test4 <- data.frame(t4.s.t, t4.h.t, t4.fh.t, t4.n.t, t4.f.t)
+
+plotlayers <- function() {
+  p <- ggplot(test1, aes(x=season, y=test4[,1], color = "a. Station")) + 
+    geom_line() + ggtitle("Temperature in different 25mm constructions (rainy day)") + 
+    ylab("Temperature (degrees Celsius)") + xlab("Date") +
+    theme(panel.background = element_rect(fill = 'white'),
+          panel.border = element_rect(color="black", size = 0.2, fill = NA),
+          plot.title = element_text(vjust=1.8, face="bold"),
+          axis.title.x=element_text(vjust=0.01)) +
+    geom_line(aes(y = test4[,2], color = "b. Holes"), 
+              linetype="longdash", alpha = 0.9) +
+    geom_line(aes(y = test4[,3], color = "c. Foil & holes"),
+              , alpha = 0.9, linetype="dashed") + ylim(c(15,35)) +
+    geom_line(aes(y = test4[,4], color = "d. Nothing"),
+              linetype="longdash", alpha = 0.9) +
+    geom_line(aes(y = test4[,5], color = "e. Foil"),
+              linetype="dashed", alpha = 0.9) +
+    scale_color_manual(name  ="Legend", 
+                       values=c("red", "lightblue", "blue", "lightgreen", "green")) 
+  return(p)
+}
+plotlayers()
+
+##------------------------------5th test (small tubes)------------------------------------##
+
+exp.time4(exp = "Exp6", type = "Station", unit = "Temp", start.m  = "15/07/14 00:00:00",
+          end.m    = "15/07/14 23:59:00")
+test5.station.temp <- as.matrix(est.obs)
+exp.time4(exp = "Exp6", type = "Holes", unit = "Temp", start.m  = "15/07/14 00:00:00",
+          end.m    = "15/07/14 23:59:00")
+test5.holes.temp <- as.matrix(est.obs)  
+exp.time4(exp = "Exp6", type = "Holesfoil", unit = "Temp", start.m  = "15/07/14 00:00:00",
+          end.m    = "15/07/14 23:59:00")
+test5.foilwh.temp <- as.matrix(est.obs)  
+exp.time4(exp = "Exp6", type = "Nothing", unit = "Temp", start.m  = "15/07/14 00:00:00",
+          end.m    = "15/07/14 23:59:00")
+test5.nothing.temp <- as.matrix(est.obs)
+exp.time4(exp = "Exp6", type = "Foil", unit = "Temp", start.m  = "15/07/14 00:00:00",
+          end.m    = "15/07/14 23:59:00")
+test5.foil.temp <- as.matrix(est.obs)
+
+t5.s.t  <- rowMeans(test5.station.temp)
+t5.h.t  <- rowMeans(test5.holes.temp)
+t5.fh.t <- rowMeans(test5.foilwh.temp)
+t5.n.t  <- rowMeans(test5.nothing.temp)
+t5.f.t  <- rowMeans(test5.foil.temp)
+
+
+test5 <- data.frame(t5.s.t, t5.h.t, t5.fh.t, t5.n.t, t5.f.t)
+
+plotlayers <- function() {
+  p <- ggplot(test1, aes(x=season, y=test5[,1], color = "a. Station")) + 
+    geom_line() + ggtitle("Temperature in different 25mm constructions (sunny day)") + 
+    ylab("Temperature (degrees Celsius)") + xlab("Date") +
+    theme(panel.background = element_rect(fill = 'white'),
+          panel.border = element_rect(color="black", size = 0.2, fill = NA),
+          plot.title = element_text(vjust=1.8, face="bold"),
+          axis.title.x=element_text(vjust=0.01)) +
+    geom_line(aes(y = test5[,2], color = "b. Holes"), 
+              linetype="longdash", alpha = 0.9) +
+    geom_line(aes(y = test5[,3], color = "c. Foil & holes"),
+              , alpha = 0.9, linetype="dashed") + ylim(c(15,35)) +
+    geom_line(aes(y = test5[,4], color = "d. Nothing"),
+              linetype="longdash", alpha = 0.9) +
+    geom_line(aes(y = test5[,5], color = "e. Foil"),
+              linetype="dashed", alpha = 0.9) +
+    scale_color_manual(name  ="Legend", 
+              values=c("red", "lightblue", "blue", "lightgreen", "green")) 
+  return(p)
+}
+  plotlayers()
+
+summary(test5)
--- data/Experiments/.Exp2Functions.R
+++ data/Experiments/.Exp2Functions.R
@@ -50,3 +50,56 @@ exp.time3    <- function(exp      = "Exp2",
     est.obs  <<- est.obs[3:(length(type.dir)+2)]
 }
 ##--------------------------------------------------------------------------##
+
+exp.time4    <- function(exp      = "Exp3",
+                         type     = "Station",
+                         rowskip  =  19, 
+                         time     = "Date.Time", 
+                         val      = "Value",
+                         format.m = "%d/%m/%y %H:%M:%S",
+                         start.m  = "03/07/14 00:00:00",
+                         end.m    = "03/07/14 23:59:00",
+                         time.un  = "hours",
+                         time.dif = 1,
+                         unit     = "Temp",
+                         na.est   = na.approx)
+{
+  
+  ## 
+  require(zoo)
+  require(ggplot2)
+  
+  start    <- as.POSIXct(start.m, format = format.m)
+  end      <- as.POSIXct(end.m, format = format.m)
+  season   <<- seq(from = start, to = end, 
+                   by = as.difftime(time.dif, units = time.un))  
+  index    <- zoo(1, season) 
+  seas     <- index
+  
+  ##    
+  type.dir <- dir(paste(exp,"/",unit,"/",type, sep =""), pattern =".csv")
+  type.zoo <- list() 
+  
+  ##
+  for (i in 1:length(type.dir)) {
+    v  <- subset(read.csv(paste(exp, "/", unit, "/", type, "/", 
+                                type.dir[i], sep =""), skip = rowskip, 
+                          header = TRUE), select = c(time, val)) 
+    v$Date.Time <- strptime(v$Date.Time, format.m)
+    vc <- as.character(v$Date.Time)
+    v$Date.Time <- as.POSIXct(vc)
+    v <- na.omit(v)                            
+    v.zoo <- zoo(v$Value, v$Date.Time)
+    type.zoo[[i]] <- v.zoo
+  }  
+  
+  ##
+  type.zoo <- type.zoo[!sapply(type.zoo, is.null)] 
+  type.df  <- do.call(merge, type.zoo)
+  
+  ## Interpolate points with NA values
+  est.obs   <- na.est(merge.zoo(index, type.df))
+  est.obs   <- as.data.frame(merge.zoo(index, est.obs, all=FALSE))   
+  est.obs  <<- est.obs[3:(length(type.dir)+2)]
+}
+##--------------------------------------------------------------------------##
